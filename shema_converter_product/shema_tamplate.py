c06f1eb9393966d7741965627e950ee8306f91f9
dmitriykodolov <kodolovdv@gmail.com>
2022-08-24 09:21:34 +0300

    Commit for schema_converter

--- shema_converter_product/shema_tamplate.py
+++ shema_converter_product/shema_tamplate.py
@@ -1,54 +1,38 @@
 import sqlite3
 import json
+from product_objects import get_from_title, get_db_columns, connection
 
 
 
-def json_generated(db_name, table_name, column_=None, name_=None):
-    connection = sqlite3.connect(db_name)
-    cursor = connection.cursor()
-    if column_ and name_:
-        query = '''
-        SELECT * FROM {table_name} WHERE {column_} LIKE ?
-        '''
-        get = cursor.execute(query.format(table_name=table_name, column_=column_), (name_,))
-    else:
-        query = '''
-        SELECT * FROM {table_name}
-        '''
-        get = cursor.execute(query.format(table_name=table_name))
-    s = get.fetchall()
         
-    for row in s:
-        with open('schema.json', 'a', newline='\n') as file:
-            dict_ = {
-                "@context": "http://schema.org/",
-                "@type": "Product",
-                "name": row[2],
-                "image": row[7],
-                "description": row[4],
-                "brand": {
-                    "@type": "Brand",
-                    "name": row[1]
-                },
-                "offers": {
-                    "@type": "Offer",
-                    "priceCurrency": "RUB",
-                    "price": row[6],
-                    "url": row[8],
-                    "availability": "https://schema.org/InStock",
-                    "itemCondition": "https://schema.org/NewCondition"
-                },
-                "aggregateRating": {
-                    "@type": "AggregateRating",
-                    "ratingValue": row[5],
-                    "ratingCount": "1",
-                    "worstRating": "50",
-                    "bestRating": "100"
-                }
+for row in get_from_title(''):
+    with open('schema2.json', 'a', newline='\n') as file:
+        dict_ = {
+            "@context": "http://schema.org/",
+            "@type": "Product",
+            "name": row[2],
+            "image": row[7],
+            "description": row[4],
+            "brand": {
+                "@type": "Brand",
+                "name": row[1]
+            },
+            "offers": {
+                "@type": "Offer",
+                "priceCurrency": "RUB",
+                "price": row[6],
+                "url": row[8],
+                "availability": "https://schema.org/InStock",
+                "itemCondition": "https://schema.org/NewCondition"
+            },
+            "aggregateRating": {
+                "@type": "AggregateRating",
+                "ratingValue": row[5],
+                "ratingCount": "1",
+                "worstRating": "50",
+                "bestRating": "100"
             }
-            json_ = json.dumps(dict_, indent=4, ensure_ascii=False)
-            file.write(json_)
-            file.write('\n\n\n\n')
-
-
-json_generated('wtdb', 'winetime', 'brand', 'Chateau Margaux')
+        }
+        json_ = json.dumps(dict_, indent=4, ensure_ascii=False)
+        file.write(json_)
+        file.write('\n\n\n\n')
